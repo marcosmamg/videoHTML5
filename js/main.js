fe5af7d68c177bac4348e514b9cc0b486848f7b2
Roberto Cordero <robertodcordero@hotmail.com>
2017-08-31 16:00:42 +0000

    Enhancement structure and new behaviour when the file upload and video recorder are used together

--- js/main.js
+++ js/main.js
@@ -1,8 +1,5 @@
 'use strict';
 
-if (!navigator.getUserMedia) {
-    fallback();
-}
 // window.isSecureContext could be used for Chrome
 var isSecureOrigin = location.protocol === 'https:' ||
 location.hostname === 'localhost';
@@ -61,118 +58,29 @@ MediaHelper.prototype.getMimeCodec = function()
   return mimeCodec;
 }
 
-// Global variables for GoogleAuth object, auth status.
-var uploadedFile;
+MediaHelper.prototype.isWebRTCSupported = function() {
+  return !!(navigator.getUserMedia || 
+            navigator.webkitGetUserMedia ||
+            navigator.mozGetUserMedia || 
+            navigator.msGetUserMedia);
+}
 
-/* globals MediaRecorder */
-var mediaSource;
-var mediaRecorder;
-var recordedBlobs;
-var sourceBuffer;
-var recording = false;
-var timeRecorded = 0;
-var intervalTimer;
-var recordedSize = 0;
-
-var gumVideo = document.querySelector('.videoRecorder video');
-var recordedVideo = document.querySelector('.videoPreview video');
-var maxTime = 0;
-
-function handleSuccess(stream) {
-  var mediaHelper = new MediaHelper();
-  window.stream = stream;
-  if (mediaHelper.getURL()) {
-    gumVideo.src = mediaHelper.getURL().createObjectURL(stream);
-  } else {
-    gumVideo.src = stream;
-  }
+function VideoPlayer(videoRecorder, videoPreview, fileUploader, settings) {
+  this.isRecorderEnabled = false;
+  this.mediaSource = null;
+  this.mediaRecorderInst = null;
+  this.recordedBlobs = [];
+  this.sourceBuffer = null;
+  this.maxDuration = 0;
   
-  mediaSource = new MediaSource();
-  mediaSource.addEventListener('sourceopen', handleSourceOpen, false);
-  
-  videoPlayer.showRecorder();
-  
-}
+  this.videoRecorderCollectingDataInterval = 10;
+  this.stream = null;
 
-function handleError(error) {
-  fallback();
-}
-
-function fallback(e) {
-  $("#videoSettings").addClass("hidden");
-  $(".videoRecorder").addClass("hidden");
-}
-
-function handleSourceOpen(event) {
-  sourceBuffer = mediaSource.addSourceBuffer('video/mp4; codecs="vp9"');
-}
-
-///////////////
-var stream;
-
-// Call handleAuthClick function when user clicks on "Authorize" button.
-  $("#select-file-button").on("click", function (e) {
-    $("#select-file").click();
-  });
-
-  $(".uploadFile").on("click", function (e) {
-    //defineRequest();
-  });
-  
-  $("#select-file").on("change", function (e) {
-    var mediaHelper = new MediaHelper();
-    uploadedFile = $("#select-file").prop("files")[0];
-    
-    if(mediaHelper.getAcceptedUploadFileTypes().indexOf(uploadedFile.type)==-1)
-    {
-      alert("File format not supported : '" + uploadedFile.type + "'");
-    } 
-    else 
-    {
-      if(mediaHelper.getURL())
-      {
-        $("#previewUploadedVideo").css("display","block").attr("src", mediaHelper.getURL().createObjectURL(uploadedFile));
-      }
-    }
-  });
-
-  $(".displayResolution").on("click", function(e) { 
-    var mediaHelper = new MediaHelper();
-    e.preventDefault(); 
-    e.stopPropagation(); 
-    
-    getMedia(mediaHelper.getVideoSettings()[$(e.target).data("name")]);
-
-  } );
-
-  function getMedia(constraints) {
-    if (stream) {
-      stream.getTracks().forEach(function(track) {
-        track.stop();
-      });
-    }
-  
-    navigator.mediaDevices.getUserMedia(constraints).
-              then(handleSuccess).
-              catch(fallback);
-  };
-
-function handleDataAvailable(event) {
-  if (event.data && event.data.size > 0) {
-    recordedBlobs.push(event.data);
-    recordedSize += event.data.size;
-  }
-}
-
-function handleStop(event) {
-  console.log('Recorder stopped: ', event);
-}
-
-function VideoPlayer(videoRecorder, videoPreview, settings) {
-  this.recordedSize = 0;
+  this.recording = false;
   this.timeRecorded = 0;
   this.intervalTimer = 0;
   
+  this.fileUploader = fileUploader;
   this.videoRecorder = videoRecorder;
   this.videoPreview = videoPreview;
   this.settings = settings;
@@ -186,14 +94,105 @@ function VideoPlayer(videoRecorder, videoPreview, settings) {
   this.sendVideoButton = videoPreview.find("button");
   
   this.toggleButton.on("click", this.toggleRecording.bind(this));
-  this.closeButton.on("click", this.showRecorder.bind(this));
+  this.closeButton.on("click", (e) => { this._setupRecorder(this.recorderSettings); this.hidePreview(); this.showRecorder(); });
   this.preview.on("error", function(ev) { alert('Your browser can not play\n\n' + ev.target.src + '\n\n media clip. event: ' + JSON.stringify(ev)); });
   
   this.mediaHelper = new MediaHelper();
+  this.recorderSettings = null;
+
+  this.settings.find(".displayResolution").on("click", (e) => { 
+    e.preventDefault(); 
+    e.stopPropagation(); 
+    
+    this.recorderSettings = this.mediaHelper.getVideoSettings()[$(e.target).data("name")];
+    
+    this._setupRecorder(this.recorderSettings);
+  } );
+  
+  this.fileUploader.find(".uploadButton").on("click", function(e) {
+    $(this).siblings("input[type='file']").trigger("click");
+  });
+
+  this.fileUploader.find("input[type='file']").on("change", (e) => {
+    var uploadedFile = $(e.target).prop("files")[0];
+    
+    if(this.mediaHelper.getAcceptedUploadFileTypes().indexOf(uploadedFile.type)==-1)
+    {
+      alert("File format not supported : '" + uploadedFile.type + "'");
+    } 
+    else 
+    {
+      if(this.mediaHelper.getURL())
+      {
+        this.hideRecorder();
+        this.isRecorderEnabled = false;
+        
+        this.preview.attr("src", this.mediaHelper.getURL().createObjectURL(uploadedFile));
+        this.showPreview();
+      }
+    }
+  });
+  
+  this._initialize();
+  
+  return this;
+}
+
+// Compatibility Check
+VideoPlayer.prototype._initialize = function() {
+  if (!this.mediaHelper.isWebRTCSupported()) {
+      this._fallback();
+  }
+}
+
+// Media Handlers
+VideoPlayer.prototype._setupRecorder = function(constraints) {
+    if (this.stream) {
+        this.stream.getTracks().forEach(function(track) {
+          track.stop();
+        });
+    }
+  
+    navigator.mediaDevices.getUserMedia(constraints).
+              then(this._handleSuccess.bind(this)).
+              catch(this._fallback.bind(this));
+};
+
+VideoPlayer.prototype._tearRecorder = function() {
+    if (this.stream) {
+        this.stream.getTracks().forEach(function(track) {
+          track.stop();
+        });
+    }
+    
+    this.stream = null;
+    this.isRecorderEnabled = false;
+}
+
+VideoPlayer.prototype._handleSuccess = function(stream) {
+  this.isRecorderEnabled = true;
+  this.stream = stream;
+  if (this.mediaHelper.getURL()) {
+    this.recorder.get(0).src = this.mediaHelper.getURL().createObjectURL(stream);
+  } else {
+    this.recorder.get(0).src = stream;
+  }
+  
+  this.mediaSource = new MediaSource();
+  this.mediaSource.addEventListener('sourceopen', 
+                               (e) => this.sourceBuffer = this.mediaSource.addSourceBuffer('video/mp4; codecs="vp9"'), 
+                               false);
+  
+  this.showRecorder();
+}
+
+VideoPlayer.prototype._fallback = function(e) {
+  this.settings.addClass("hidden");
+  this.videoRecorder.addClass("hidden");
 }
 
 VideoPlayer.prototype.toggleRecording = function(e) {
-    if (!recording) {
+    if (!this.recording) {
       if(this.isReadyToRecord())
       {
         this.startRecording();            
@@ -205,15 +204,72 @@ VideoPlayer.prototype.toggleRecording = function(e) {
     }
   }
 
+VideoPlayer.prototype.initializeMediaRecorder = function()
+{
+  var options = this.mediaHelper.getMimeCodec();
+  
+  try {
+    this.mediaRecorderInst = new MediaRecorder(this.stream, options);
+  } catch (e) {
+    alert('Exception while creating MediaRecorder: ' + e + '. mimeType: ' + options.mimeType);
+    return;
+  }
+  
+  this.mediaRecorderInst.onstop = (e) => { console.log('Recorder stopped: ', e); }
+  this.mediaRecorderInst.ondataavailable = (e) => { 
+                                    if (e.data && e.data.size > 0) {
+                                      this.recordedBlobs.push(e.data);
+                                    }
+                                  }
+  
+  this.mediaRecorderInst.start(this.videoRecorderCollectingDataInterval); // collect 10ms of data
+}
+
+VideoPlayer.prototype.startRecording = function() {
+  if(this.isRecorderEnabled)
+  {
+    this.recordedBlobs = [];
+    this.recording = true;
+    
+    this.initializeMediaRecorder();
+    this.startTimer();
+    this.hidePreview();
+    this.showRecorder();    
+  }
+}
+
+VideoPlayer.prototype.stopRecording = function() {
+  if(this.isRecorderEnabled)
+  {
+    this.mediaRecorderInst.stop();
+    this.recording = false;
+    
+    this.hideRecorder();
+    this.stopTimer();
+    this.playPreview();
+    this.showPreview();    
+  }
+}
+
+
+/// Timer Handlers
+VideoPlayer.prototype.updateMaxTimeToRecord = function() 
+{
+  this.maxDuration = 0;
+  var maxDuration = this.settings.find("input.maxDuration");
+  if(maxDuration.length > 0) {
+    this.maxDuration = parseInt(maxDuration.val(), 10);
+  } 
+}
+
 VideoPlayer.prototype.updateTimer = function() {
   var now = new Date();
   var diff = now - this.timeRecorded;
-  var secs = parseInt(diff/1000, 10);
-  var mins = parseInt(secs/60, 10);
-  secs = parseInt((diff - mins * 60000)/1000, 10)
+  var mins = parseInt(diff/60000, 10);
+  var secs = parseInt((diff - mins * 60000)/1000, 10)
     
   this.timer.html(this.mediaHelper.formatTime(mins) + ":" + this.mediaHelper.formatTime(secs));
-  if(mins >= maxTime)
+  if(mins >= this.maxDuration)
   {
     this.stopRecording(); 
   }
@@ -228,7 +284,6 @@ VideoPlayer.prototype.startTimer = function()
   this.intervalTimer = setInterval(this.updateTimer.bind(this), 1000);
   this.toggleButton.css("background-color", "red");
   this.timer.removeClass("hidden");
-//  document.getElementById("recorderButton").style.backgroundColor = "red";
 }
 
 VideoPlayer.prototype.stopTimer = function() {
@@ -240,43 +295,12 @@ VideoPlayer.prototype.stopTimer = function() {
   this.settings.removeClass("hidden");
 }
 
-VideoPlayer.prototype.initializeMediaRecorder = function()
-{
-  var options = this.mediaHelper.getMimeCodec();
-  
-  try {
-    mediaRecorder = new MediaRecorder(window.stream, options);
-  } catch (e) {
-    alert('Exception while creating MediaRecorder: ' + e + '. mimeType: ' + options.mimeType);
-    return;
-  }
-  
-  mediaRecorder.onstop = handleStop;
-  mediaRecorder.ondataavailable = handleDataAvailable;
-  mediaRecorder.start(10); // collect 10ms of data
-}
-
-VideoPlayer.prototype.startRecording = function() {
-  recordedBlobs = [];
-  recording = true;
-  this.recordedSize = 0;
-  this.initializeMediaRecorder();
-  this.startTimer();
-}
-
-VideoPlayer.prototype.stopRecording = function() {
-  mediaRecorder.stop();
-  recording = false;
-  
-  this.stopTimer();
-  this.playPreview();
-  this.showPreview();
-}
+/// Recorder Handlers
 
 VideoPlayer.prototype.isReadyToRecord = function()
 {
-  maxTime = document.getElementById("maxTimeToRecord").value;
-  if (maxTime == "")
+  this.updateMaxTimeToRecord();
+  if (this.maxDuration == 0)
   {
     alert("Select max time to record");
     return false;
@@ -284,32 +308,50 @@ VideoPlayer.prototype.isReadyToRecord = function()
   return true;
 }
 
+VideoPlayer.prototype.showRecorder = function(){
+  
+  if(this.isRecorderEnabled)
+  {
+    this.recorder.get(0).play();
+    
+    this.settings.removeClass("hidden");
+    this.videoRecorder.removeClass("hidden");
+  }
+}
+
+VideoPlayer.prototype.hideRecorder = function() {
+  if(this.isRecorderEnabled)
+  {
+    this.recorder.get(0).pause();
+    this._tearRecorder();
+
+    this.settings.addClass("hidden");
+    this.videoRecorder.addClass("hidden");
+  }
+}
+
+/// Preview Handlers
+
 VideoPlayer.prototype.playPreview = function() {
-  var superBuffer = new Blob(recordedBlobs, {type: 'video/mp4'});
+  var superBuffer = new Blob(this.recordedBlobs, {type: 'video/mp4'});
   this.preview.get(0).src = this.mediaHelper.getURL().createObjectURL(superBuffer);
   this.preview.get(0).controls = true;
 }
 
-VideoPlayer.prototype.showRecorder = function(){
-  this.recorder.get(0).play();
-  this.preview.get(0).pause();
-  
-  this.settings.removeClass("hidden");
-  this.videoRecorder.removeClass("hidden");
 
+VideoPlayer.prototype.showPreview = function() {
+  this.preview.get(0).play();
+  this.videoPreview.removeClass("hidden");
+}
+
+VideoPlayer.prototype.hidePreview = function(){
+  this.preview.get(0).pause();
   this.videoPreview.addClass("hidden");
 }
 
-VideoPlayer.prototype.showPreview = function() {
-  this.recorder.get(0).pause();
-  this.preview.get(0).play();
-  
-  this.videoPreview.removeClass("hidden");
-  
-  this.settings.addClass("hidden");
-  this.videoRecorder.addClass("hidden");
-}
+
 
 var videoPlayer = new VideoPlayer($(".videoRecorder"), 
                                   $(".videoPreview"), 
-                                  $("#videoSettings"));
+                                  $(".fileUpload"),
+                                  $(".videoSettings"));
