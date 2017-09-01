a7005183a76706b1306777919006025ed43bc2f2
Marcos Moreno <marcosmorenomamg@gmail.com>
2017-09-01 16:29:46 +0000

    fixing upload post parameters

--- js/main.js
+++ js/main.js
@@ -370,16 +370,14 @@ app.controller('MyCtrl', ['$scope', '$http', '$timeout', '$compile', 'Upload', f
   $scope.uploadS3 = function() {
     var file = generateFile();
     file.upload = Upload.upload({
-      url: 'https://videowisp.s3.amazonaws.com/',
+      url: 'https://videowisp2.s3-us-west-2.amazonaws.com/',
       method: 'POST',
       data: {
         key: file.name,
+        AWSAccessKeyId: 'AKIAINNO3M3HLXDDRLEQ',
         acl: 'public-read',
-        policy: 'eyAiZXhwaXJhdGlvbiI6ICIyMDIwLTEyLTMwVDEyOjAwOjAwLjAwMFoiLAogICJjb25kaXRpb25zIjogWwogICAgeyJidWNrZXQiOiAidmlkZW93aXNwIn0sCiAgICBbInN0YXJ0cy13aXRoIiwgIiRrZXkiLCAiIl0sCiAgICB7ImFjbCI6ICJwdWJsaWMtcmVhZCJ9LCAgICAKICAgIFsic3RhcnRzLXdpdGgiLCAiJENvbnRlbnQtVHlwZSIsICIiXSwKICAgIHsieC1hbXotbWV0YS11dWlkIjogIjE0MzY1MTIzNjUxMjc0In0sCiAgICB7IngtYW16LXNlcnZlci1zaWRlLWVuY3J5cHRpb24iOiAiQUVTMjU2In0sCiAgICBbInN0YXJ0cy13aXRoIiwgIiR4LWFtei1tZXRhLXRhZyIsICIiXSwKICAgIHsieC1hbXotY3JlZGVudGlhbCI6ICJBS0lBSjJNRTVRUFBJSDRSNktTQS8yMDE3MDgzMS91cy1lYXN0LTIvczMvYXdzNF9yZXF1ZXN0In0sCiAgICB7IngtYW16LWFsZ29yaXRobSI6ICJBV1M0LUhNQUMtU0hBMjU2In0sCiAgICB7IngtYW16LWRhdGUiOiAiMjAxNzA4MzFUMDAwMDAwWiIgfQogIF0KfQ==',
-        'X-Amz-Signature': 'o7rU9oK3Js+3/YEmDPaJQzeJ36g=',
-        'X-Amz-Credential' : 'AKIAJ2ME5QPPIH4R6KSA/20170831/us-east-2/s3/aws4_request',
-        'X-Amz-Algorithm': 'AWS4-HMAC-SHA256',
-        'X-Amz-Date': '20170831T000000Z',
+        policy: 'ewogICJleHBpcmF0aW9uIjogIjIwMjAtMDEtMDFUMDA6MDA6MDBaIiwKICAiY29uZGl0aW9ucyI6IFsKICAgIHsiYnVja2V0IjogInZpZGVvd2lzcDIifSwKICAgIFsic3RhcnRzLXdpdGgiLCAiJGtleSIsICIiXSwKICAgIHsiYWNsIjogInB1YmxpYy1yZWFkIn0sCiAgICBbInN0YXJ0cy13aXRoIiwgIiRDb250ZW50LVR5cGUiLCAiIl0sCiAgICBbImNvbnRlbnQtbGVuZ3RoLXJhbmdlIiwgMCwgNTI0Mjg4MDAwXQogIF0KfQ==',
+        signature: 'M5+QfbP8ELaMOt3zjSMS8djMEIA=',
         "Content-Type": '',
         file: file,
         filename: 'test.mp4',
